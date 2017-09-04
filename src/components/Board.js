88d975ec7e7ef2f0c02176f6a4841605bf8a65cd
Greg Fisher <me@gnfisher.com>
2017-09-04 10:28:48 -0300

    Render all the grid spaces

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import Gridspace from './Gridspace';
 
 export class Board extends Component {
+  createGridspaces = (key) => {
+    return <Gridspace key={key} />;
+  }
+
+  renderInnerGrid = () => {
+    const gridLetters = [
+      "A", "B", "C", "D", "E",
+      "F", "G", "H", "I", "J",
+      "K", "L", "M", "N", "O",
+      "P", "Q", "R", "S", "T",
+      "U", "V", "X", "Y"
+    ];
+
+    return gridLetters.map(key => this.createGridspaces(key));
+  }
+
   render() {
     return (
       <div id="board">
         <Gridspace key="TRANSIT" long={true} />
         <div className="inner-grid">
-
+          {this.renderInnerGrid()}
         </div>
-        <Gridspace key="Z" />
+        <Gridspace key="Z" long={true} />
       </div>
     );
   }
