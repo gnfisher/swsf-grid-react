0e9fbeed9d4c1fadccc96d8dd164d3dccaff889f
Greg Fisher <me@gnfisher.com>
2017-09-05 13:16:35 -0300

    make sure no errors key in unitsobject in test

--- src/lib/UnitImporer.test.js
+++ src/lib/UnitImporer.test.js
@@ -1,77 +1,78 @@
 import UnitImporter, { ParseUtility } from './UnitImporter';
 
 describe('UnitImporter', () => {
-  // it('should create a unit object from each valid line of input', () => {
-  //   const textInput = "B>N: ISD Headhunter\nA>N: ISD Avenger";
-  //   const units = UnitImporter.getUnitsFromText(textInput);
-  //   expect(Object.keys(units).length).toBe(2);
-  // });
+  it('should create a unit object from each valid line of input', () => {
+    const textInput = "B>N: ISD Headhunter\nA>N: ISD Avenger";
+    const units = UnitImporter.getUnitsFromText(textInput);
+    expect(Object.keys(units)).not.toContain('errors');
+    expect(Object.keys(units).length).toBe(2);
+  });
 
-  // it('should skip blank lines', () => {
-  //   const textInput = "A>B: ISD Gummy\n\nA>B: ISD Bears";
-  //   const units = UnitImporter.getUnitsFromText(textInput);
-  //   expect(Object.keys(units).length).toBe(2);
-  // });
+  it('should skip blank lines', () => {
+    const textInput = "A>N: ISD Gummy\n\nC>N: ISD Bears";
+    const units = UnitImporter.getUnitsFromText(textInput);
+    expect(Object.keys(units)).not.toContain('errors');
+    expect(Object.keys(units).length).toBe(2);
+  });
 
-  // it('should return errors array when location malformatted', () => {
-  //   const textInput = "32>B: ISD Gummy";
-  //   const units = UnitImporter.getUnitsFromText(textInput);
-  //   expect(units.errors.length).toBeGreaterThan(0);
-  // });
+  it('should return errors array when location malformatted', () => {
+    const textInput = "32>B: ISD Gummy";
+    const units = UnitImporter.getUnitsFromText(textInput);
+    expect(units.errors.length).toBeGreaterThan(0);
+  });
 
-  // it('should return errors array when heading malformatted', () => {
-  //   const textInput = "A>32: ISD Gummy";
-  //   const units = UnitImporter.getUnitsFromText(textInput);
-  //   expect(units.errors.length).toBeGreaterThan(0);
-  // });
+  it('should return errors array when heading malformatted', () => {
+    const textInput = "A>32: ISD Gummy";
+    const units = UnitImporter.getUnitsFromText(textInput);
+    expect(units.errors.length).toBeGreaterThan(0);
+  });
 
-  // it('should return errors array when unit type is not found', () => {
-  //   const textInput = "A>N: isd something";
-  //   const units = UnitImporter.getUnitsFromText(textInput);
-  //   expect(units.errors.length).toBeGreaterThan(0);
-  // });
+  it('should return errors array when unit type is not found', () => {
+    const textInput = "A>N: isd something";
+    const units = UnitImporter.getUnitsFromText(textInput);
+    expect(units.errors.length).toBeGreaterThan(0);
+  });
 
   it('should throw an alert and reset if more than one unit in A-Y grids', () => {
     const textInput = "A>B: ISD Gummy\nA>B: ISD Bears";
     const units = UnitImporter.getUnitsFromText(textInput);
-    console.log(units);
     expect(units.errors.length).toBeGreaterThan(0);
   });
 });
 
-// describe('ParseUtility', () => {
-//   it('#parseLocation', () => {
-//     let locationWithLetter = ParseUtility.parseLocation('A>B: ISD Headhunter');
-//     expect(locationWithLetter).toBe('A');
+describe('ParseUtility', () => {
+  it('#parseLocation', () => {
+    let locationWithLetter = ParseUtility.parseLocation('A>B: ISD Headhunter');
+    expect(locationWithLetter).toBe('A');
 
-//     let locationWithTransit = ParseUtility.parseLocation('Transit>B: ISD Headhunter');
-//     expect(locationWithTransit).toBe('TRANSIT');
-//   });
+    let locationWithTransit = ParseUtility.parseLocation('Transit>B: ISD Headhunter');
+    expect(locationWithTransit).toBe('TRANSIT');
+  });
 
-//   it('#parseHeading', () => {
-//     let heading = ParseUtility.parseHeading('A>N: ISD Headhunter');
-//     expect(heading).toBe('N');
-//   });
+  it('#parseHeading', () => {
+    let heading = ParseUtility.parseHeading('A>N: ISD Headhunter');
+    expect(heading).toBe('N');
+  });
 
-//   it('#parseUnitType', () => {
-//     let unitType = ParseUtility.parseUnitType('A>N: ISD Headhunter');
-//     expect(unitType).toBe('ISD');
+  it('#parseUnitType', () => {
+    let unitType = ParseUtility.parseUnitType('A>N: ISD Headhunter');
+    expect(unitType).toBe('ISD');
 
-//     unitType = ParseUtility.parseUnitType('A>N: ISD1 Headhunter');
-//     expect(unitType).toBe('ISD1');
+    unitType = ParseUtility.parseUnitType('A>N: ISD1 Headhunter');
+    expect(unitType).toBe('ISD1');
 
-//     unitType = ParseUtility.parseUnitType('A>N: MC80A Headhunter');
-//     expect(unitType).toBe('MC80A');
-//   });
+    unitType = ParseUtility.parseUnitType('A>N: MC80A Headhunter');
+    expect(unitType).toBe('MC80A');
+  });
 
-//   it('#parseName', () => {
-//     let name = ParseUtility.parseName('A>N: ISD Headhunter');
-//     expect(name).toBe('Headhunter');
+  it('#parseName', () => {
+    let name = ParseUtility.parseName('A>N: ISD Headhunter');
+    expect(name).toBe('Headhunter');
 
-//     name = ParseUtility.parseName("A>N: ISD Death's Head");
-//     expect(name).toBe("Death's Head");
+    name = ParseUtility.parseName("A>N: ISD Death's Head");
+    expect(name).toBe("Death's Head");
 
-//     name = ParseUtility.parseName("A>N: XW Fish (36)");
-//     expect(name).toBe("Fish (36)");
-//   });
-// });
+    name = ParseUtility.parseName("A>N: XW Fish (36)");
+    expect(name).toBe("Fish (36)");
+  });
+});
